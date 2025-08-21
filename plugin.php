4aed00baa4372c8d533b3bd88b4693886d811a3e
jeremyfelt <jeremy.felt@gmail.com>
2025-08-21 14:35:37 -0700

    Bump version 0.3.0

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  SPS CBOX SSO
  * Description:  Add CUNY SSO integration to Commons In A Box
- * Version:      0.2.2
+ * Version:      0.3.0
  * Plugin URI:   https://github.com/cuny-academic-commons/sps-cbox-sso
  * Author:       CUNY Academic Commons
  * Author URI:   https://commons.gc.cuny.edu
